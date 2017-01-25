ff8837f38953ec46f188ea9b2cdf5937caa800b1
HSJanny <hataichanok_s@yahoo.com>
2017-01-25 04:47:41 +0000

    first commit

---
+++ js/controller.js
@@ -0,0 +1,25 @@
+﻿angular.module('root.controllers', [])
+
+.controller('rootCtrl', function ($scope, $rootScope) {
+})
+
+.controller('homeCtrl', function ($scope, $rootScope, $state, $stateParams, homeService) {
+    //calling this method when user clicks at search button
+    $scope.doSearch = function () {
+        $scope.mClick = false;
+        //calling getClientData from service.js
+        homeService.getClientData($scope.clientReference).then(function (response) {
+            //set $scope.mHome to return json data
+            $scope.mHome = response.data;
+        }, function (response) {
+        });
+    };
+
+    //calling this method when user clicks at more detail button by passing index of quary that user want to see more details
+    $scope.getMoreDetails = function (index) {
+        //set mClick = true if user click at more details button 
+        $scope.mClick = true;
+        //set  $scope.mIndex equal to index parameter
+        $scope.mIndex = index;
+    };
+});
---
+++ js/service.js
@@ -0,0 +1,10 @@
+﻿angular.module('root.services', [])
+
+.factory('homeService', function ($http, $rootScope) {
+    return {
+        //perform a get request
+        getClientData: function (clientReference) {
+            return $http.get("https://login.caseblocks.com/case_blocks/search?query=client_reference:" + clientReference + "&auth_token=bDm1bzuz38bpauzzZ_-z");
+        }
+    };
+});
